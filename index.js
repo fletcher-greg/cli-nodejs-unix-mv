1dcb69a2508cfcc06149e1d07068bed7b7576ca8
fletcher-greg <fletcher.web.dev.one@gmail.com>
2019-11-19 16:13:41 +0100

    added comments

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
 const fsPromises = require("fs").promises;
-const fs = require("fs");
 
 async function main() {
+  // grab source file and destination file
   let origin = process.argv[2];
   let destination = process.argv[3];
-
+  // check to see if source and destination are same file type
   let check = validate(origin, destination);
 
   if (check !== "success") {
     console.log("not a valid file type");
     return;
   }
+  // check to see if origin exists
   let exists = await checkFileExists(origin);
   if (!exists) return;
-
+  // copy file to destination
   await fsPromises
     .copyFile(origin, destination)
     .then(() => console.log("copied"))
     .catch(() => console.log(error));
 
   try {
+    // try to delete file
     await fsPromises.unlink(origin);
   } catch (e) {
     console.log("error deleting file");
   }
 }
-
+// check to see if files are the same type
 function validate(origin, destination) {
   let oFileType = origin.split(".")[1];
   let dFileType = destination.split(".")[1];
@@ -36,6 +38,8 @@ function validate(origin, destination) {
   }
   return "error";
 }
+
+// check to see if file exists
 async function checkFileExists(origin) {
   try {
     let result = await fsPromises.open(origin);
